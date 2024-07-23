39a8b806591c2ba359f2a7ea6833844f60f210dc
Yasir Mehmood <129255451+1Yasir@users.noreply.github.com>
2024-07-23 16:18:15 +0500

    create PasswordGenerator Component

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "We created this React application to develop different React projects, allowing us to increase our knowledge of React and practice using React props and hooks."
+
+# first we build Password generator  
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import PasswordGenerator from './ccomponents/PasswordGenerator';
 
 function App() {
   return (
-    <div>App</div>
+    <div className="app">
+      <PasswordGenerator />
+    </div>
   )
 }
 
