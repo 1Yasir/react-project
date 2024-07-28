19f653d45f49fdd32f72dad1ef646f57dff70270
Yasir Mehmood <129255451+1Yasir@users.noreply.github.com>
2024-07-28 23:45:15 +0500

    add lazy load funcality

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import React from 'react';
-import PasswordGenerator from './components/PasswordGenerator';
-import FetchApiData from './components/FetchApiData';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import useFetch from './customHook/useFetch';
-import OnlineStatusCheck from './components/OnlineStatusCheck';
-import Form from './components/Form';
-import CurrencyConverter from './components/currency/CurrencyConverter';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header/Header';
 import Home from './components/Home/Home';
-import QuizApplication from './components/quizApplication/QuizApplication';
-import SideBar from './components/sidebar/SideBar';
+
+const PasswordGenerator = lazy(() => import('./components/PasswordGenerator'));
+const FetchApiData = lazy(() => import('./components/FetchApiData'));
+const CurrencyConverter = lazy(() => import('./components/currency/CurrencyConverter'));
+const QuizApplication = lazy(() => import('./components/quizApplication/QuizApplication'));
+const SideBar = lazy(() => import('./components/sidebar/SideBar'));
+
 function App() {
-  const [data] = useFetch("https://jsonplaceholder.typicode.com/posts");
-  // console.log("app", data?.length);
   return (
-
     <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route path='/' element = {<Home />} />
-        <Route path='/currency-converter' element = {<CurrencyConverter />} />
-        <Route path='/password-generator' element = {<PasswordGenerator />} />
-        <Route path='/quiz-application' element = {<QuizApplication />} />
-        <Route path='/fetch-api' element = {<FetchApiData />} />
-        <Route path='/side-bar' element = {<SideBar />} />
-      </Routes>
+      <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/currency-converter' element={<CurrencyConverter />} />
+          <Route path='/password-generator' element={<PasswordGenerator />} />
+          <Route path='/quiz-application' element={<QuizApplication />} />
+          <Route path='/fetch-api' element={<FetchApiData />} />
+          <Route path='/side-bar' element={<SideBar />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
-
-  )
+  );
 }
 
-export default App
+export default App;
